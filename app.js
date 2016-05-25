e685dc3eedd4b33d3dcb6eb6437f7f9ed428ba81
jonssmall <jonssmall@gmail.com>
2016-05-24 20:02:38 -0400

    instantiate 9 squares

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert("test");
+var Square = function (place) {
+  this.place = place; //1-9
+  this.marker = null; //"X" or "O"
+};
+
+Square.prototype.isOccupied = function() {
+	return this.marker ? true : false;
+};
+
+var board = [];
+
+for (var i = 1; i < 10; i++) {	
+	board.push(new Square(i));
+}
+console.log(board);
