1d44f925f11e86f6d2db8c889814e2ad71ec8473
jonssmall <jonssmall@gmail.com>
2016-05-24 20:18:04 -0400

    player class

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ for (var i = 1; i < 10; i++) {
 	board.push(new Square(i));
 }
 console.log(board);
+
+var Player = function(marker) {
+	this.marker = marker;
+	this.score = 0;
+};
+
+Player.prototype.placeMarker = function(square) {
+	if(square.isOccupied) {
+		//TODO: some type of error / blocked action
+	} else {
+		square.marker = this.marker;
+	}
+};
